87e1534bcf5d5f7bf6f78500a810492a6402a8af
vitugc1 <victorcarvalho13022002@gmail.com>
2022-01-29 17:24:45 -0300

    theme

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import { usePersistedState } from './hooks/usePersistedState';
+
+import { Header } from './components/Header';
+
+import light from './styles/Themes/light'
+import dark from './styles/Themes/dark'
+import GlobalStyle from './styles/global';
+
+export const App: React.FC = () => {
+    const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+    const toggleTheme = useCallback(() => {
+      setTheme(theme.title === 'light' ? dark : light);
+    }, [theme]);
+
+    return (
+      <ThemeProvider theme={theme}>
+        <div>
+          <GlobalStyle />
+          <Header toggleTheme={toggleTheme} />
+        </div>
+      </ThemeProvider>
+    )
+};
+
+
+
+
+
